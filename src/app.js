2720ebf9f512733cc9c8accc1ba9fdd27400b53e
HelloWorld017 <deu07115@gmail.com>
2016-08-20 17:05:36 +0900

    Updated app

--- src/app.js
+++ src/app.js
@@ -1,60 +1,61 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var favicon = require('serve-favicon');
+var FacebookStrategy = require('passport-facebook');
+var logger = require('morgan');
+var passport = require('passport');
+var path = require('path');
+var TwitterStrategy = require('passport-twitter');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('../routes/index');
+var meal = require('../routes/meal');
+var school = require('../routes/school');
+var user = require('../routes/user');
+
+passport.use(new TwitterStrategy({
+	consumerKey: global.config.auth.twitter.key,
+	consumerSecret: global.config.auth.twitter.secret,
+	callbackURL: global.config.auth.twitter.callbackURL
+}));
+
+passport.use(new FacebookStrategy({
+
+}));
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/meal', meal);
+app.use('/school', school);
+app.use('/user', user);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: (app.get('env') === 'development') ? err : {}
+		});
+	});
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
